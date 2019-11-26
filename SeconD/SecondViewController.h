3631de736c61e4b7ff6f3fb66dd1cc8261989f58
Maria <ffd@sd>
2019-11-26 12:46:10 +0300

    user muzhik

---
+++ SeconD/SecondViewController.h
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.h
+//  SeconD
+//
+//  Created by Student on 26.11.2019.
+//  Copyright © 2019 Pogromist LTD. All rights reserved.
+//
+
+#import "ViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SecondViewController : ViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SeconD/SecondViewController.m
@@ -0,0 +1,36 @@
+//
+//  SecondViewController.m
+//  SeconD
+//
+//  Created by Student on 26.11.2019.
+//  Copyright © 2019 Pogromist LTD. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"ЗИГ!мунд Фрейд");
+}
+
+- (IBAction)backTap:(id)sender{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
